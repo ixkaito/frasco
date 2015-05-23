54ed7890730b8c41a28fc4daa1d8e83b9d12fdb8
Kite <ixkaito@gmail.com>
2015-05-24 03:24:51 +0900

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var compass     = require('gulp-compass');
+var plumber     = require('gulp-plumber');
+var cp          = require('child_process');
+
+var messages = {
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+  browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['compass', 'jekyll-build'], function() {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('compass', function () {
+  gulp.src('assets/_sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(compass({
+      config_file: 'config.rb',
+      // comments: false,
+      css: '_site/assets/css/',
+      sass: 'assets/_sass/'
+    }))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+  gulp.watch('assets/_sass/*.scss', ['compass']);
+  gulp.watch(['index.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
