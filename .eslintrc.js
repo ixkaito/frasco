0cc1d8ed4f9f0418d32c3b98b53f2ea247bc45c2
Kite <ixkaito@gmail.com>
2020-08-10 17:29:19 +0900

    Update frasco

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'extends': ['eslint:recommended', 'google'],
+  'env': {
+    // For more environments, see here: http://eslint.org/docs/user-guide/configuring.html#specifying-environments
+    'browser': true,
+    'es6': true,
+    'jquery': true
+  },
+  'rules': {
+    // Insert custom rules here
+    // For more rules, see here: http://eslint.org/docs/rules/
+    'no-var': 'warn',
+    'require-jsdoc': 'off'
+  },
+  'parserOptions': {
+    'sourceType': 'module'
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MODE = 'production';
+const enabledSourceMap = (MODE === 'development');
+
+module.exports = {
+  mode: MODE,
+
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: enabledSourceMap,
+              plugins: [
+                require('autoprefixer')({grid: true}),
+              ],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: enabledSourceMap,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '${__dirname}/assets/css/style.css',
+    }),
+  ],
+};
